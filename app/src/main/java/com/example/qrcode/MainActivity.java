e7f4b206fd0f493b0c8682f01bdb489da6bd9909
Ivan F <ivan.fradellin@itiszuccante.edu.it>
2022-03-31 20:12:43 +0200

    Lezione del 31/3 Prima implementazione lettura NFC

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -3,23 +3,83 @@ package com.example.qrcode;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NfcAdapter nfcAdapter; // hardware che permette di accedere all'NFC
+    PendingIntent pendingIntent; // tipo promise
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        pendingIntent = PendingIntent
+                            .getActivity(this,
+                                    0,
+                                    new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                                    0);
     }
 
+    /*
+    Devo gestire anche quando la mia applicazione viene riaperta, va in pausa ecc.
+    Quindi sovrascrivo i metodi onPause, onResume e onIntent
+     */
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        nfcAdapter.enableForegroundDispatch(this,
+                pendingIntent,
+                null,
+                null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        setIntent(intent);
+        resolveIntent(intent);
+
+    }
+
+    private void resolveIntent(Intent intent) {
+        String action = intent.getAction(); // azione collegata all'NFC
+
+        if (action.equals(NfcAdapter.ACTION_TAG_DISCOVERED) ||
+                action.equals(NfcAdapter.ACTION_TECH_DISCOVERED) ||
+                action.equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+            Toast.makeText(this, "E' successo qualcosa", Toast.LENGTH_LONG).show();
+        }
+    }
 
     public void scanClick(View v) {
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
