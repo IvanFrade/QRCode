55531a597f2ce16df3b807b98d0cf91e73f6f427
Studente Zuccante <studente@lap2-ubu20.cz.lan>
2022-03-31 19:26:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.qrcode;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void scanClick(View v) {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setOrientationLocked(true);
+
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+
+        TextView debugTextView = findViewById(R.id.txt_debug);
+        debugTextView.setText(result.getContents());
+    }
+}
