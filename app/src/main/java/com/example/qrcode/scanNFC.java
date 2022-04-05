8d702f1e7a48b67842b1c417400a21fdda3f40c1
Ivan F <ivan.fradellin@itiszuccante.edu.it>
2022-04-05 22:00:08 +0200

    Lezione 04/05

--- app/src/main/java/com/example/qrcode/scanNFC.java
+++ app/src/main/java/com/example/qrcode/scanNFC.java
@@ -1,24 +1,22 @@
 package com.example.qrcode;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
 import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
 import android.nfc.tech.NfcA;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class scanNFC extends AppCompatActivity {
 
     private NfcAdapter nfcAdapter; // hardware che permette di accedere all'NFC
     PendingIntent pendingIntent; // tipo promise
@@ -26,20 +24,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scan_nfc);
 
-        /*nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         pendingIntent = PendingIntent
-                            .getActivity(this,
-                                    0,
-                                    new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
-                                    0);*/
+                .getActivity(this,
+                        0,
+                        new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                        0);
     }
 
     /*
     Devo gestire anche quando la mia applicazione viene riaperta, va in pausa ecc.
     Quindi sovrascrivo i metodi onPause, onResume e onIntent
-
+     */
 
     @Override
     protected void onResume() {
@@ -77,30 +75,40 @@ public class MainActivity extends AppCompatActivity {
                 action.equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 
-            Toast.makeText(this, "E' successo qualcosa", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, readContentTag(tag), Toast.LENGTH_LONG).show();
         }
-    }*/
-
-    public void nfcScanClick(View v) {
-        Intent intent = new Intent(this, scanNFC.class);
-        startActivity(intent);
     }
 
-    public void scanClick(View v) {
-        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
-        intentIntegrator.setOrientationLocked(true);
+    private String readContentTag(Tag tag) {
+        String toReturn = "";
+        String[] tagTech = tag.getTechList();
 
-        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-        intentIntegrator.initiateScan();
-    }
+        for (int i = 0; i < tagTech.length; i++) {
+            if (tagTech[i].equals(MifareUltralight.class.getName())) {
+                MifareUltralight mifareUltralight = MifareUltralight.get(tag);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+                try {
+                    mifareUltralight.connect();
 
-        IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+                    // primi 4 byte non sono da leggere
+                    byte[] bs = mifareUltralight.readPages(4);
+                    toReturn += new String(bs, 4, 12);
 
-        TextView debugTextView = findViewById(R.id.txt_debug);
-        debugTextView.setText(result.getContents());
+                    for (int k = 8; k < 28; k += 4) {
+                        bs = mifareUltralight.readPages(k);
+
+                        int l = 0;
+                        while (bs[l] != -2 && l < bs.length) l++;
+
+                        toReturn += new String(bs, 0, l);
+                        if (l < bs.length) k = 40;
+                    }
+                } catch (IOException e) {
+                    return e.getMessage();
+                }
+            }
+        }
+
+        return toReturn;
     }
 }
